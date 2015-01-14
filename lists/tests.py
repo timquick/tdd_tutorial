8a92b5e1b5b3c46d0fb033f317d646fc2cace80a
Tim Quick <timquick@yahoo.com>
2015-01-14 12:46:00 -0500

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -17,3 +17,38 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
         
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = "A new list item"
+        
+        response = home_page(request)
+        
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
+        
+
+from lists.models import Item
+class ItemModelTest(TestCase):
+    
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+        
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+        
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        
--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
         
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+        
     def test_cat_start_a_list_and_retrieve_it_later(self):
 
         # Edith has heard about a cool new online to-do app.
@@ -35,16 +40,18 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
-
         #  There is still a text box inviting her to add another item.  She
         # enters "Use peacock feathers to make a fly" 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        
+        
+        
         self.fail('Finish the test!')
         
         # The page updates aagain, and now shows both items on her list
