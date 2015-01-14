3cf0bfecd523a1d2e7d56856163ecb9ef256715d
Tim Quick <timquick@yahoo.com>
2015-01-14 17:35:00 -0500

    refactor

--- superlists/urls.py
+++ superlists/urls.py
@@ -6,6 +6,5 @@ urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/', include('lists.urls')),
     # url(r'^blog/', include('blog.urls')),
-
     #url(r'^admin/', include(admin.site.urls)),
 )
