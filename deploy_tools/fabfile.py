101057d509de139137ffe1a846a303d32418924c
Tim Quick <timquick@yahoo.com>
2015-01-21 20:51:23 -0500

    Fabric Deployment

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -32,7 +32,7 @@ def _update_settings(source_folder, site_name):
     sed(settings_path,
         'ALLOWED_HOSTS = .+$',
         'ALLOWED_HOSTS = ["%s"]' % (site_name,))
-    secret_key_file = source_folder + '/superlists/secret_key.py'
+    secret_key_file = source_folder + '/superlists/.secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
